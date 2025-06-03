ffcb178d0ca1ffc80f1ce14e1c6fe6013d4eeb7f
ngn <ngn@ngn.tf>
2025-06-03 17:54:47 +0300

    remove the release script and add the tag script
    Signed-off-by: ngn <ngn@ngn.tf>

---
+++ scripts/tag.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+# import common functions
+if [ ! -f "scripts/common.sh" ]; then
+  echo "You should run this script in the root of the repository"
+  exit 1
+fi
+
+source scripts/common.sh
+
+git tag "${VERSION}" && success "Tagged the version ${VERSION}" && exit 0
+
+fail "Failed to tag the version ${VERSION}"
+exit 1
